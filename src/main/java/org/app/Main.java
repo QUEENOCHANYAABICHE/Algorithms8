1c401c7e2c01f521231574c7ecac90fbfdac9c80
Decagon <deca@Decagons-MacBook-Air.local>
2023-07-30 08:55:52 +0100

    Algorithms

---
+++ src/main/java/org/app/Main.java
@@ -0,0 +1,20 @@
+package org.app;
+
+public class Main{
+    public static void main(String[] args) {
+        //DISPLAY GEOMETRIC PROGRESSION
+//GP = a+ ar + ar^2 + ar^3 + ar^4 + ar^5;
+        int start =3;
+        int difference=3;
+        int length=10;
+        int term =1;
+        for(int i=0; i<=10; i++){
+            term = term * difference;
+            System.out.print(term+ ",");
+        }
+
+    }
+}
+
+
+//https://github.com/QUEENOCHANYAABICHE/Algorithms8.git
